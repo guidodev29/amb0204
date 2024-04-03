b15b6d3a249d00a8a4971e15d6473ab5a77f3c3f
unknown <UFG@CC6-12.ufg.local>
2024-04-02 20:00:54 -0600

    Practica de Fragmento azul terminada

---
+++ app/src/main/java/edu/ufg/sv/fis/amb/hdezguido_fragmentos/MainActivity.kt
@@ -0,0 +1,23 @@
+package edu.ufg.sv.fis.amb.hdezguido_fragmentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace<primer_fragmento>(R.id.fragment_container_view)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
+
+
+    }
+
+}
